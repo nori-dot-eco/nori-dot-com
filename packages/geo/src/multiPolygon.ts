5546811e67f1a567eb1a20546e6900038ab30659
jaycen horton <jaycen.horton@gmail.com>
2019-03-06 23:55:18 -0700

    type-ify everything, clean up internal function logic

--- packages/geo/src/multiPolygon.ts
+++ packages/geo/src/multiPolygon.ts
@@ -1,22 +1,10 @@
-const turf = require('@turf/turf');
-const fs = require('fs');
-const path = require('path');
-const Wkt = require('wicket');
+import * as turf from '@turf/turf';
+import { Feature } from '@turf/helpers';
+import { FeatureCollection, MultiPolygon, Position } from 'geojson';
 
 // todo use jsdoc
-// todo type-ify
 
-const writeFile = ({ outputFile, geoCollection }) => {
-  console.log(
-    '\nThe GeoJSON to be written to the output file is:\n\n',
-    JSON.stringify(geoCollection)
-  );
-  fs.writeFile(outputFile, JSON.stringify(geoCollection), error =>
-    error ? console.log(error) : null
-  );
-};
-
-const createGeoJSON = coordinates => {
+const createGeoJSONFeature = (coordinates: Position[]): Feature => {
   const geometry = {
     type: 'Polygon',
     coordinates: [coordinates],
@@ -25,83 +13,104 @@ const createGeoJSON = coordinates => {
   return feature;
 };
 
-const getWKT = ({ geometry }) => {
-  const wkt = new Wkt.Wkt();
-  wkt.fromObject(geometry);
-  return wkt.write();
+const closestPointsInPolys = (
+  poly1: Position[],
+  poly2: Position[]
+): {
+  closest1: Position[];
+  closest2: Position[];
+  closestIndex1: number;
+  closestIndex2: number;
+} => {
+  let shortest1 = 100000000000000000000;
+  let shortest2 = turf.distance(poly1[0], poly2[0]);
+  let closest1 = [poly1[0], poly2[0]];
+  let closest2 = [poly1[0], poly2[0]];
+  let closestIndex1 = 0;
+  let closestIndex2 = 0;
+  poly1.forEach((poly1Coords, i) => {
+    poly2.forEach((_, j) => {
+      const distance = turf.distance(poly1[i], poly2[j]);
+      if (distance < shortest1) {
+        closest1 = [poly1Coords];
+        shortest1 = distance;
+        closestIndex1 = i;
+      }
+    });
+  });
+  console.log('\nThe closest point in polygon 1 to polygon 2 is', closest1);
+  poly2.forEach((poly2Coords, i) => {
+    poly1.forEach((_, j) => {
+      const distance = turf.distance(poly1[j], poly2[i]);
+      if (distance < shortest2) {
+        closest2 = [poly2Coords];
+        shortest2 = distance;
+        closestIndex2 = i;
+      }
+    });
+  });
+  console.log('The closest point in polygon 1 to polygon 2 is', closest2);
+  return {
+    closest1,
+    closest2,
+    closestIndex1,
+    closestIndex2,
+  };
 };
 
-const drawShortestPathBetweenPolys = ({ coordinates }) => {
-  return turf.multiPolygon([coordinates]).geometry.coordinates.map(polygons => {
+const drawShortestPathBetweenPolys = ({ coordinatesGroup }): Feature[] =>
+  turf.multiPolygon([coordinatesGroup]).geometry.coordinates.map(polygons => {
     const [poly1, poly2] = polygons;
-    let shortest1 = 100000000000000000000;
-    let shortest2 = turf.distance(poly1[0], poly2[0]);
-    let closest1 = [poly1[0], poly2[0]];
-    let closest2 = [poly1[0], poly2[0]];
-    let closestIndex1 = 0;
-    let closestIndex2 = 0;
-    poly1.map((coordinates, i) => {
-      for (let j = 0; j < poly2.length; j++) {
-        const distance = turf.distance(poly1[i], poly2[j]);
-        if (distance < shortest1) {
-          closest1 = coordinates;
-          shortest1 = distance;
-          closestIndex1 = i;
-        }
-      }
-    });
-    console.log('\nThe closest point in polygon 1 to polygon 2 is', closest1);
-    poly2.map((coordinates, i) => {
-      for (let j = 0; j < poly1.length; j++) {
-        const distance = turf.distance(poly1[j], poly2[i]);
-        if (distance < shortest2) {
-          closest2 = coordinates;
-          shortest2 = distance;
-          closestIndex2 = i;
-        }
-      }
-    });
-    console.log('The closest point in polygon 1 to polygon 2 is', closest2);
-    const poly1End = poly1.splice(0, closestIndex1);
-    const newPoly1 = poly1
-      .concat(poly1End)
-      .concat([[closest1[0] + 0.00000001, closest1[1]]]); // todo need to add when negative, sub when positive
-    const poly2End = poly2.splice(0, closestIndex2).concat([closest2]);
-    let poly2FirstCoord = poly2.shift();
-    poly2FirstCoord = [poly2FirstCoord[0] + 0.00000001, poly2FirstCoord[1]]; // todo need to add when negative, sub when positive
-    const newPoly2 = [poly2FirstCoord]
+    const {
+      closest1,
+      closest2,
+      closestIndex1,
+      closestIndex2,
+    } = closestPointsInPolys(poly1, poly2);
+
+    // create polygon ends for the new geom
+    const poly1FinalLatShifted =
+      closest1[0][0] < 0
+        ? closest1[0][0] + 0.00000001
+        : closest1[0][0] - 0.00000001;
+    const poly1End = poly1
+      .splice(0, closestIndex1)
+      .concat([[poly1FinalLatShifted, closest1[0][1]]]);
+    const poly2End = poly2.splice(0, closestIndex2).concat(closest2);
+
+    // redraw polygons
+    const redrawnPoly1 = poly1.concat(poly1End);
+    let poly2FirstCoords = poly2.shift();
+    const poly2FirstLatShifted =
+      poly2FirstCoords[0] < 0
+        ? poly2FirstCoords[0] + 0.00000001
+        : poly2FirstCoords[0] - 0.00000001;
+    poly2FirstCoords = [poly2FirstLatShifted, poly2FirstCoords[1]];
+    const redrawnPoly2 = [poly2FirstCoords]
       .concat(poly2)
       .concat(poly2End)
-      .concat([closest1]);
-    const newGeo = [...newPoly1, ...newPoly2];
+      .concat(closest1);
+    const redrawnGeometryWithPath = [...redrawnPoly1, ...redrawnPoly2];
     console.log(
       'Polygons redrawn with connection between',
       closest1,
       'and',
       closest2
     );
-
-    const collection = createGeoJSON(newGeo);
-    return collection;
+    return createGeoJSONFeature(redrawnGeometryWithPath);
   });
-};
 
 const multiPolygonToPolygon = (
-  input = './test.geojson',
-  output = './out.geojson'
-) => {
+  featureCollection: FeatureCollection<MultiPolygon>
+): Feature => {
   const {
     features: [
       {
-        geometry,
-        geometry: { coordinates },
+        geometry: { coordinates: coordinatesGroup },
       },
     ],
-  } = JSON.parse(fs.readFileSync(path.join(__dirname, input), 'utf8'));
-  console.log(coordinates);
-  const [geoCollection] = drawShortestPathBetweenPolys({ coordinates });
-  writeFile({ outputFile: path.join(__dirname, output), geoCollection });
-  const wkt = getWKT({ geometry });
-  console.log('\n\nThe WKT is:\n\n', JSON.stringify(wkt), '\n');
+  } = featureCollection;
+  const [geoJson] = drawShortestPathBetweenPolys({ coordinatesGroup });
+  return geoJson;
 };
 export default multiPolygonToPolygon;
