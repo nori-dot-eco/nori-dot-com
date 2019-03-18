3c8a144e4686ecda01cb98aacb266628817d24dd
jaycen horton <jaycen.horton@gmail.com>
2019-03-18 10:52:16 -0700

    debug checkpoint

--- packages/geo/src/__tests__/multiPolygon.test.ts
+++ packages/geo/src/__tests__/multiPolygon.test.ts
@@ -1,14 +1,62 @@
+import * as turf from '@turf/turf';
 import { getType } from '@turf/invariant';
 import { randomPolygon } from '@turf/random';
 import { multiPolygonToPolygon } from '../index';
 
-const createRandomGeoJson = () => {
-  const poly1 = randomPolygon(1, {
-    num_vertices: Math.floor(Math.random() * 6) + 4,
-  }).features[0].geometry.coordinates[0];
-  const poly2 = randomPolygon(1, {
-    num_vertices: Math.floor(Math.random() * 6) + 4,
-  }).features[0].geometry.coordinates[0];
+// vertical poly test
+// const geoJson = {
+//   type: 'FeatureCollection',
+//   features: [
+//     {
+//       type: 'Feature',
+//       properties: {},
+//       geometry: {
+//         type: 'MultiPolygon',
+//         coordinates: [
+//           [
+//             [-88.9201405865274, 30.96039815570841],
+//             [-88.92423930428181, 30.916333114046935],
+//             [-89.04607230985769, 30.920807534148008],
+//             [-89.07016867720978, 30.962247844991765],
+//             [-88.9201405865274, 30.96039815570841],
+//           ],
+//           [
+//             [-88.7913169593782, 33.198751006041924],
+//             [-88.84775490200774, 32.985736640198624],
+//             [-88.84627052219702, 33.03248084008883],
+//             [-89.01663814267641, 33.225629405253976],
+//             [-88.7913169593782, 33.198751006041924],
+//           ],
+//         ],
+//       },
+//     },
+//   ],
+// };
+
+const createRandomPolygon = ({ bbox }) => {
+  let polygon;
+  do {
+    polygon = randomPolygon(1, {
+      num_vertices: Math.floor(Math.random() * 20) + 8,
+      bbox,
+      max_radial_length: 0.2,
+    });
+  } while (
+    // turf has a weird quirk that will create invalid
+    // polygons with kinks (edges that intersect its interior;
+    // this will keep creating random polygons until that is not the case)
+    turf.kinks(polygon.features[0]).features.length
+  );
+  return { polygon, coords: polygon.features[0].geometry.coordinates[0] };
+};
+
+const createRandomGeoJson = boundingBoxes => {
+  const polygons = boundingBoxes.map(
+    bbox =>
+      createRandomPolygon({
+        bbox,
+      }).coords
+  );
   const geoJson = {
     type: 'FeatureCollection',
     features: [
@@ -17,18 +65,58 @@ const createRandomGeoJson = () => {
         properties: {},
         geometry: {
           type: 'MultiPolygon',
-          coordinates: [poly1, poly2],
+          coordinates: [...polygons],
         },
       },
     ],
   };
-  return { geoJson, poly1, poly2 };
+  return { geoJson, polygons };
 };
 
+// describe('multiPolygonToPolygon', () => {
+//   let redrawnPolygon, geoJson, polygons;
+//   beforeAll(() => {
+//     ({ geoJson, polygons } = createRandomGeoJson([
+//       [-89, 36, -88.5, 36.9],
+//       [-92, 37, -90.5, 38],
+//       [-94, 36, -92.5, 36.9],
+//       [-97, 36, -95.5, 36.9],
+//       [-100, 36, -98.5, 36.9],
+//       [-103, 36, -101.5, 36.9],
+//       [-106, 36, -104.5, 36.9],
+//     ]));
+//     redrawnPolygon = multiPolygonToPolygon(<any>(<unknown>geoJson));
+//   });
+//   it('converts a multi-polygon with two disconnected polygons into a singular polygon with a short path drawn between them', () => {
+//     expect(redrawnPolygon).toEqual(
+//       expect.objectContaining({
+//         geometry: {
+//           coordinates: expect.arrayContaining([
+//             expect.arrayContaining([...polygons[0], ...polygons[1]]),
+//           ]),
+//           type: 'Polygon',
+//         },
+//         properties: {},
+//         type: 'Feature',
+//       })
+//     );
+//   });
+//   describe('after converting a multi-polygon to a polygon', () => {
+//     it('will be a polygon', () => {
+//       console.log(JSON.stringify(redrawnPolygon));
+//       expect(getType(redrawnPolygon)).toBe('Polygon');
+//     });
+//   });
+// });
+
 describe('multiPolygonToPolygon', () => {
-  let redrawnPolygon, geoJson, poly1, poly2;
+  let redrawnPolygon, geoJson, polygons;
   beforeAll(() => {
-    ({ geoJson, poly1, poly2 } = createRandomGeoJson());
+    ({ geoJson, polygons } = createRandomGeoJson([
+      [-89, 30, -88.5, 31.5],
+      [-89, 36, -88.5, 36.9],
+      [-88.5, 33, -88.0, 33.5],
+    ]));
     redrawnPolygon = multiPolygonToPolygon(<any>(<unknown>geoJson));
   });
   it('converts a multi-polygon with two disconnected polygons into a singular polygon with a short path drawn between them', () => {
@@ -36,7 +124,7 @@ describe('multiPolygonToPolygon', () => {
       expect.objectContaining({
         geometry: {
           coordinates: expect.arrayContaining([
-            expect.arrayContaining([...poly1, ...poly2]),
+            expect.arrayContaining([...polygons[0], ...polygons[1]]),
           ]),
           type: 'Polygon',
         },
@@ -46,8 +134,8 @@ describe('multiPolygonToPolygon', () => {
     );
   });
   describe('after converting a multi-polygon to a polygon', () => {
-    it('will be a polygon', async () => {
-      await true;
+    it('will be a polygon', () => {
+      console.log(JSON.stringify(redrawnPolygon));
       expect(getType(redrawnPolygon)).toBe('Polygon');
     });
   });
--- packages/geo/src/multiPolygon.ts
+++ packages/geo/src/multiPolygon.ts
@@ -1,5 +1,6 @@
 import * as turf from '@turf/turf';
-import { Feature } from '@turf/helpers';
+import booleanDisjoint from '@turf/boolean-disjoint';
+import { Feature, lineString } from '@turf/helpers';
 import { FeatureCollection, MultiPolygon, Position } from 'geojson';
 
 // todo use jsdoc
@@ -16,44 +17,55 @@ const closestPointsInPolys = (
   poly1: Position[],
   poly2: Position[]
 ): {
-  closest1: Position[];
-  closest2: Position[];
-  closestIndex1: number;
-  closestIndex2: number;
+  closestCoordsInBothPolys: Position[];
+  // closest2: Position[];
+  closestIndexToPoly1InPoly2: number;
+  closestIndexToPoly2InPoly1: number;
 } => {
-  let shortest1 = 100000000000000000000;
-  let shortest2 = turf.distance(poly1[0], poly2[0]);
-  let closest1 = [poly1[0], poly2[0]];
-  let closest2 = [poly1[0], poly2[0]];
-  let closestIndex1 = 0;
-  let closestIndex2 = 0;
+  let shortestPathBetweenPolys = turf.distance(poly1[0], poly2[0]);
+  // let shortest2 = turf.distance(poly2[0], poly1[0]);
+  let closestCoordsInBothPolys = [poly1[0], poly2[0]];
+  // let closest2 = [poly1[0], poly2[0]];
+  let closestIndexToPoly1InPoly2 = 0;
+  let closestIndexToPoly2InPoly1 = 0;
+  // let closestIndex2 = 0;
+  const polygonGeoFeature1 = turf.polygon([poly1]);
+  // const polygonGeoFeature2 = turf.polygon([poly2]);
+
   poly1.forEach((poly1Coords, i) => {
     poly2.forEach((_, j) => {
       const distance = turf.distance(poly1[i], poly2[j]);
-      if (distance < shortest1) {
-        closest1 = [poly1Coords];
-        shortest1 = distance;
-        closestIndex1 = i;
+      const line = turf.lineString([poly1[i], poly2[j]]);
+      if (
+        distance < shortestPathBetweenPolys &&
+        !booleanDisjoint(line, polygonGeoFeature1)
+      ) {
+        closestCoordsInBothPolys = [poly1[i], poly2[j]];
+        shortestPathBetweenPolys = distance;
+        closestIndexToPoly2InPoly1 = i;
+        closestIndexToPoly1InPoly2 = j;
       }
     });
   });
-  console.log('\nThe closest point in polygon 1 to polygon 2 is', closest1);
-  poly2.forEach((poly2Coords, i) => {
-    poly1.forEach((_, j) => {
-      const distance = turf.distance(poly1[j], poly2[i]);
-      if (distance < shortest2) {
-        closest2 = [poly2Coords];
-        shortest2 = distance;
-        closestIndex2 = i;
-      }
-    });
-  });
-  console.log('The closest point in polygon 1 to polygon 2 is', closest2);
+  // console.log('\nThe closest point in polygon 1 to polygon 2 is', closest1);
+  // poly2.forEach((poly2Coords, i) => {
+  //   poly1.forEach((_, j) => {
+  //     const distance = turf.distance(poly1[j], poly2[i]);
+  //     const line = turf.lineString([poly1[j], poly2[i]]);
+  //     // console.log([poly1, poly2]);
+  //     if (distance < shortest2 && !booleanDisjoint(line, polygonGeoFeature2)) {
+  //       closest2 = [poly2Coords];
+  //       shortest2 = distance;
+  //       closestIndex2 = i;
+  //     }
+  //   });
+  // });
+  // console.log('The closest point in polygon 2 to polygon 1 is', closest2);
   return {
-    closest1,
-    closest2,
-    closestIndex1,
-    closestIndex2,
+    closestCoordsInBothPolys,
+    // closest2,
+    closestIndexToPoly1InPoly2,
+    closestIndexToPoly2InPoly1,
   };
 };
 
@@ -61,55 +73,119 @@ const drawShortestPathBetweenPolys = ({ coordinatesGroup }): Feature[] =>
   turf.multiPolygon([coordinatesGroup]).geometry.coordinates.map(polygons => {
     const [poly1, poly2] = polygons;
     const {
-      closest1,
-      closest2,
-      closestIndex1,
-      closestIndex2,
+      closestCoordsInBothPolys,
+      // closest2,
+      closestIndexToPoly2InPoly1,
+      closestIndexToPoly1InPoly2,
     } = closestPointsInPolys(poly1, poly2);
-
+    poly1.pop();
+    poly2.pop();
+    console.log('closestCoordsInBothPolys', closestCoordsInBothPolys);
     // create polygon ends for the new geom
+    const poly1FinalLongShifted =
+      turf.bearing(closestCoordsInBothPolys[0], closestCoordsInBothPolys[1]) >=
+      -90
+        ? // closest1[0][0] < closest2[0][0]
+          closestCoordsInBothPolys[0][0] - 0.0002
+        : closestCoordsInBothPolys[0][0] + 0.0002;
     const poly1FinalLatShifted =
-      closest1[0][0] < 0
-        ? closest1[0][0] + 0.00000001
-        : closest1[0][0] - 0.00000001;
-    const poly1End = poly1
-      .splice(0, closestIndex1)
-      .concat([[poly1FinalLatShifted, closest1[0][1]]]);
-    const poly2End = poly2.splice(0, closestIndex2).concat(closest2);
+      turf.bearing(closestCoordsInBothPolys[0], closestCoordsInBothPolys[1]) <=
+      -90
+        ? // closest1[0][1] > closest2[0][1]
+          closestCoordsInBothPolys[0][1] + 0.0002
+        : closestCoordsInBothPolys[0][1] - 0.0002;
+    console.log(
+      'turf.bearing(closestCoordsInBothPolys[0], closestCoordsInBothPolys[1]) >= -90',
+      turf.bearing(closestCoordsInBothPolys[0], closestCoordsInBothPolys[1]) >=
+        -90
+    );
+    console.log('[poly1FinalLongShifted, poly1FinalLatShifted]', [
+      poly1FinalLongShifted,
+      poly1FinalLatShifted,
+    ]);
+    const poly1End = poly1.splice(0, closestIndexToPoly2InPoly1);
+    const poly2End = poly2.splice(0, closestIndexToPoly1InPoly2);
+    // .concat([poly2[closestIndexToPoly1InPoly2]]);
+    console.log('poly1End', poly1End);
+    console.log('poly2End', poly2End);
 
     // redraw polygons
-    const redrawnPoly1 = poly1.concat(poly1End);
+    const redrawnPoly1 = poly1
+      .concat(poly1End)
+      .concat([[poly1FinalLongShifted, poly1FinalLatShifted]]);
     let poly2FirstCoords = poly2.shift();
+    const poly2FirstLongShifted =
+      turf.bearing(closestCoordsInBothPolys[1], closestCoordsInBothPolys[0]) <=
+      90
+        ? // closest2[0][0] > closest1[0][0]
+          closestCoordsInBothPolys[1][0] - 0.0001
+        : closestCoordsInBothPolys[1][0] + 0.0001;
+
     const poly2FirstLatShifted =
-      poly2FirstCoords[0] < 0
-        ? poly2FirstCoords[0] + 0.00000001
-        : poly2FirstCoords[0] - 0.00000001;
-    poly2FirstCoords = [poly2FirstLatShifted, poly2FirstCoords[1]];
+      turf.bearing(closestCoordsInBothPolys[1], closestCoordsInBothPolys[0]) >=
+      90
+        ? // closest2[0][1] < closest1[0][1]
+          closestCoordsInBothPolys[1][1] + 0.0001
+        : closestCoordsInBothPolys[1][1] - 0.0001;
+    // console.log(
+    //   ' turf.bearing(closest2[0], closest1[0]) >= 90',
+    //   turf.bearing(closest2[0], closestCoordsInBothPolys[0]) >= 90
+    // );
+    poly2FirstCoords = [poly2FirstLongShifted, poly2FirstLatShifted];
+    console.log('poly2FirstCoords', poly2FirstCoords);
     const redrawnPoly2 = [poly2FirstCoords]
       .concat(poly2)
       .concat(poly2End)
-      .concat(closest1);
-    const redrawnGeometryWithPath = [...redrawnPoly1, ...redrawnPoly2];
-    console.log(
-      'Polygons redrawn with connection between',
-      closest1,
-      'and',
-      closest2
-    );
+      .concat([closestCoordsInBothPolys[1]]);
+    const redrawnGeometryWithPath = [
+      ...redrawnPoly1,
+      ...redrawnPoly2,
+      redrawnPoly1[0],
+    ];
+    console.log('redrawnGeometryWithPath', redrawnGeometryWithPath);
+    // console.log(
+    //   'Polygons redrawn with connection between',
+    //   closestCoordsInBothPolys,
+    //   'and',
+    //   closest2
+    // );
     return createGeoJSONFeature(redrawnGeometryWithPath);
   });
 
 const multiPolygonToPolygon = (
   featureCollection: FeatureCollection<MultiPolygon>
 ): Feature => {
-  const {
+  let {
     features: [
       {
         geometry: { coordinates: coordinatesGroup },
       },
     ],
   } = featureCollection;
-  const [geoJson] = drawShortestPathBetweenPolys({ coordinatesGroup });
+  // console.log(coordinatesGroup, [coordinatesGroup[0][1]]);
+  let geoJson;
+  for (let i = 0; i < coordinatesGroup.length; i++) {
+    console.log('[coordinatesGroup[0], coordinatesGroup[1]]', [
+      coordinatesGroup[0],
+      coordinatesGroup[1],
+    ]);
+    [geoJson] = drawShortestPathBetweenPolys({
+      coordinatesGroup: [coordinatesGroup[0], coordinatesGroup[1]],
+    });
+    const {
+      geometry: { coordinates: redrawnCoordinates },
+    } = geoJson;
+    // console.log(redrawnCoordinates);
+    coordinatesGroup.shift();
+    coordinatesGroup.shift();
+    coordinatesGroup = [
+      ...redrawnCoordinates,
+      ...(coordinatesGroup.length ? coordinatesGroup : []),
+    ];
+    // console.log(coordinatesGroup.length, coordinatesGroup);
+  }
+  // console.log(coordinatesGroup);
+
   return geoJson;
 };
 export default multiPolygonToPolygon;
