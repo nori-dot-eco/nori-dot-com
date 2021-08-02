bae175769176352632780f9239bccd9b8d5f9406
amiecorso <amie@nori.com>
2021-08-02 14:05:18 -0700

    export ErrorCollector and ContextualError

--- packages/project/src/index.ts
+++ packages/project/src/index.ts
@@ -4,3 +4,4 @@ export * from './utils/convertFromV2ToV1';
 export * from './utils/v1-utils';
 export * from './utils/convertFromGgitToProject';
 export * from './legacy-specifications';
+export * from './utils/ErrorCollector';
