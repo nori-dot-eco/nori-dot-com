acf8932803fd27d97d2d980c7a6c3d706f6444ba
jaycenhorton <jaycen.horton@gmail.com>
2021-10-14 07:51:52 -0700

    rewrite math fns with better type support

--- packages/math/src/math.ts
+++ packages/math/src/math.ts
@@ -1,26 +1,20 @@
-import * as math from 'mathjs';
+import { bignumber, round } from 'mathjs';
 
-export const multiply = (x: number, y: number): number =>
-  math.number(
-    math.multiply(math.bignumber(x), math.bignumber(y)) as math.BigNumber
-  ) as number;
+type BigNumberArg = Parameters<typeof bignumber>[0];
 
-export const divide = (x: number, y: number): number =>
-  math.number(
-    math.divide(math.bignumber(x), math.bignumber(y)) as math.BigNumber
-  ) as number;
+export const multiply = (x: BigNumberArg, y: BigNumberArg): number =>
+  bignumber(x).mul(bignumber(y)).toNumber();
 
-export const add = (x: number, y: number): number =>
-  math.number(
-    math.add(math.bignumber(x), math.bignumber(y)) as math.BigNumber
-  ) as number;
+export const divide = (x: BigNumberArg, y: BigNumberArg): number =>
+  bignumber(x).div(bignumber(y)).toNumber();
 
-export const subtract = (x: number, y: number): number =>
-  math.number(
-    math.subtract(math.bignumber(x), math.bignumber(y)) as math.BigNumber
-  ) as number;
+export const add = (x: BigNumberArg, y: BigNumberArg): number =>
+  bignumber(x).add(bignumber(y)).toNumber();
 
-export const roundToDigit = (number: number, numberOfDigits: number): number =>
-  math.number(
-    math.round(math.bignumber(number), math.bignumber(numberOfDigits))
-  ) as number;
+export const subtract = (x: BigNumberArg, y: BigNumberArg): number =>
+  bignumber(x).sub(bignumber(y)).toNumber();
+
+export const roundToDigit = (
+  number: BigNumberArg,
+  numberOfDigits: number
+): number => round(bignumber(number), numberOfDigits).toNumber();
