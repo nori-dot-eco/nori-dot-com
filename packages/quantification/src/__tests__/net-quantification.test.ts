bb4a119207f466c5077faea944ba3ae2b6fb9b3e
mrdave-dev <dave@nori.com>
2022-06-03 11:59:24 -0700

    Adds describe block, modifies test

--- packages/quantification/src/__tests__/net-quantification.test.ts
+++ packages/quantification/src/__tests__/net-quantification.test.ts
@@ -4,137 +4,158 @@ import { getNetQuantificationProjection } from '../net-quantification';
  * If you'd like to see the output from the algorithm, add `console` as the second
  * parameter to `getNetQuantificationProjection`
  */
+describe('getNetQuantificationProjection', () => {
+  it('should return a total of 18 NRT', () => {
+    const testData = [
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': -30,
+          '2015': 10,
+          '2017': 9,
+          '2014': 0,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': 10,
+          '2017': 9,
+          '2018': 0,
+          '2015': 10,
+        },
+      },
+    ];
 
-it('should return a total of 18 NRT', () => {
-  const testData = [
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': -30,
-        '2015': 10,
-        '2017': 9,
+    expect(getNetQuantificationProjection(testData)).toStrictEqual([
+      {
         '2014': 0,
-      },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': 10,
-        '2017': 9,
+        '2015': 0,
+        '2016': 0,
+        '2017': 0,
         '2018': 0,
+      },
+      {
+        '2014': 0,
         '2015': 10,
+        '2016': 0,
+        '2017': 8,
+        '2018': 0,
       },
-    },
-  ];
+    ]);
+  });
 
-  expect(getNetQuantificationProjection(testData)).toStrictEqual([
-    {
-      '2014': 0,
-      '2015': 0,
-      '2016': 0,
-      '2017': 0,
-      '2018': 0,
-    },
-    {
-      '2014': 0,
-      '2015': 10,
-      '2016': 0,
-      '2017': 8,
-      '2018': 0,
-    },
-  ]);
-});
+  it('should handle a single year', () => {
+    const testData = [
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': 5,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': 10,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': 15,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': -20,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2016': 2,
+        },
+      },
+    ];
 
-it('should handle a single year', () => {
-  const testData = [
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': 5,
-      },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': 10,
-      },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': 15,
-      },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': -20,
-      },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
-        '2016': 2,
-      },
-    },
-  ];
-
-  expect(getNetQuantificationProjection(testData)).toStrictEqual([
-    {
-      '2016': 0,
-    },
-    {
-      '2016': 0,
-    },
-    {
-      '2016': 12,
-    },
-    {
-      '2016': 0,
-    },
-    {
-      '2016': 0,
-    },
-  ]);
-});
-
-it('should persist a negative amount in the column it originated from', () => {
-  const testData = [
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+    expect(getNetQuantificationProjection(testData)).toStrictEqual([
+      {
         '2016': 0,
       },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+      {
+        '2016': 0,
+      },
+      {
+        '2016': 12,
+      },
+      {
+        '2016': 0,
+      },
+      {
+        '2016': 0,
+      },
+    ]);
+  });
+
+  it('should persist a left-over negative amount in the cell it originated from', () => {
+    const testData = [
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2015': 0,
+          '2016': 0,
+          '2017': 0,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2015': 0,
+          '2016': -50,
+          '2017': 0,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2015': 0,
+          '2016': -40,
+          '2017': 0,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2015': 0,
+          '2016': -20,
+          '2017': 0,
+        },
+      },
+      {
+        somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+          '2015': 0,
+          '2016': 0,
+          '2017': 0,
+        },
+      },
+    ];
+
+    expect(getNetQuantificationProjection(testData)).toStrictEqual([
+      {
+        '2015': 0,
+        '2016': 0,
+        '2017': 0,
+      },
+      {
+        '2015': 0,
         '2016': -50,
+        '2017': 0,
       },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+      {
+        '2015': 0,
         '2016': -40,
+        '2017': 0,
       },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+      {
+        '2015': 0,
         '2016': -20,
+        '2017': 0,
       },
-    },
-    {
-      somscAnnualDifferencesBetweenFutureAndBaselineScenarios: {
+      {
+        '2015': 0,
         '2016': 0,
+        '2017': 0,
       },
-    },
-  ];
-
-  expect(getNetQuantificationProjection(testData)).toStrictEqual([
-    {
-      '2016': 0,
-    },
-    {
-      '2016': -50,
-    },
-    {
-      '2016': -40,
-    },
-    {
-      '2016': -20,
-    },
-    {
-      '2016': 0,
-    },
-  ]);
+    ]);
+  });
 });
