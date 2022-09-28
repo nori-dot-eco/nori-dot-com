26a38b767f853dd54d6305c1c87bdb3d5ee464c4
scottynomad <scott@nori.com>
2022-09-28 13:52:48 -0700

    Fixed typos

--- packages/project/src/v4-specification.ts
+++ packages/project/src/v4-specification.ts
@@ -936,7 +936,7 @@ export interface Field {
    */
   earliestEvidenceYear: number;
   /**
-   * Used to indicate the the availalable physical evidence does not corroborate the provided switch year.
+   * Used to indicate that the available physical evidence does not corroborate the provided switch year.
    * 
    * @example <caption>If the physical evidence of a switch to no-till was an undated photo of the no-till planter.</caption>
    * ```js
