1555e290daffe040fafb1f4997e40d45740624b7
Scott Hoffberg Wilson <scottynomad@users.noreply.github.com>
2022-12-08 14:23:23 -0800

    Added a cli script entrypoint to the quantification logic. (#225)

--- packages/quantification/README.md
+++ packages/quantification/README.md
@@ -7,3 +7,25 @@ Quantification utilities
 ```ts
 import { getQuantificationSummary } from '@nori-dot-com/quantification';
 ```
+
+## CLI Usage
+
+### Setup
+```sh
+$ yarn install
+```
+
+### For a multi-field results file.
+
+```sh
+
+$ yarn quantify multi results.json --maxGrandfatherableYears 4
+```
+
+### For an older single-field results file.
+
+i.e. one that may have multiple polygons from the same boundary split out and run as separate fields.
+
+```sh
+$ yarn quantify single results.json --maxGrandfatherableYears 4
+```
---
+++ packages/quantification/src/scripts/cli.ts
@@ -0,0 +1,63 @@
+#!/usr/bin/env ts-node
+
+import {
+  getQuantificationSummaries,
+  getQuantificationSummary,
+} from '../quantification';
+import * as yargs from 'yargs';
+import * as fs from 'fs';
+
+const quantificationArgs = (yargs: yargs.Argv<{}>): yargs.Argv<{}> => {
+  yargs.positional('input', {
+    type: 'string',
+    describe: 'the soil metrics output file',
+  });
+  yargs.options('maxGrandfatherableYears', {
+    type: 'number',
+    default: 4,
+    describe: 'maximum number of grandfatherable years for the project.',
+  });
+  return yargs;
+};
+
+yargs
+  .scriptName('quantify')
+  .usage('$0 <cmd> [args]')
+  .command(
+    'multi [input]',
+    'Runs Nori quantification logic on a multi-field (i.e. python) results file.',
+    quantificationArgs,
+    async (argv) => {
+      const data = fs.readFileSync(argv.input as string, 'utf8');
+      const results = await getQuantificationSummaries({
+        data: JSON.parse(data),
+        maxNumberGrandfatheredYearsForProject:
+          argv.maxGrandfatherableYears as number,
+      });
+      console.log(JSON.stringify(results, null, 4));
+    }
+  )
+  .command(
+    'single [input]',
+    'Runs Nori quantification logic on a single-field (i.e. sheets) results file.',
+    (yargs) => {
+      quantificationArgs(yargs).options('quantifyAsOfYear', {
+        type: 'number',
+        describe:
+          'Force the year to grandfather as of. Defaults to the current year',
+      });
+    },
+    async (argv) => {
+      const data = fs.readFileSync(argv.input as string, 'utf8');
+      const results = await getQuantificationSummary({
+        data: JSON.parse(data),
+        maxNumberGrandfatheredYearsForProject:
+          argv.maxGrandfatherableYears as number,
+        quantifyAsOfYear: argv.quantifyAsOfYear as number,
+      });
+      console.log(JSON.stringify(results, null, 4));
+    }
+  )
+  .demandCommand(2)
+  .strict()
+  .help().argv;
