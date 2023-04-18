624cf0149341433a7f012c235f6793597ecd0def
kyleclark171 <kyle@nori.com>
2023-04-18 13:59:33 -0700

    Updated the ggit index file

--- packages/ggit/src/index.ts
+++ packages/ggit/src/index.ts
@@ -4,5 +4,7 @@ export * from './upload';
 export * from './api';
 export * from './auth';
 export * from './validation';
+export * from './ggit-error';
+export * from './modeled-ggit-response';
 export * as Input from './input';
 export * as Output from './output';
