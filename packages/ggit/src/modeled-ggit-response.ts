39ddfa54166097861bdc388d30a6fc4b546b54d0
kyleclark171 <kyle@nori.com>
2023-04-17 17:30:50 -0700

    Updated modeled ggit response format

--- packages/ggit/src/modeled-ggit-response.ts
+++ packages/ggit/src/modeled-ggit-response.ts
@@ -1,12 +1,12 @@
 import { FlatGGITError } from './ggit-error';
-import { Daycent, MapUnit, OutputFile, ErrorResponse } from './output';
+import { MapUnit, OutputFile, ErrorResponse } from './output';
 
 export class ModeledGGITResponse {
-    public readonly day: Daycent<MapUnit>;
+    public readonly daycent: OutputFile<MapUnit>;
     public readonly errors: FlatGGITError[];
 
     constructor(response: OutputFile<MapUnit> | ErrorResponse) {
-        this.day = (response as OutputFile<MapUnit>)?.Day;
+        this.daycent = (response as OutputFile<MapUnit>);
         this.errors = FlatGGITError.fromErrorResponse(response as ErrorResponse);
     }
 }
