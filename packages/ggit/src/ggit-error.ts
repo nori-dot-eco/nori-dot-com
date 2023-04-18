6432be6ffbfc279f4278097b57d95ea3295ff7bf
kyleclark171 <kyle@nori.com>
2023-04-18 15:17:57 -0700

    Added unit tests

--- packages/ggit/src/ggit-error.ts
+++ packages/ggit/src/ggit-error.ts
@@ -1,80 +1,82 @@
-import { ModelRunErrorCollection, ErrorResponse } from './output'
+import type { ModelRunErrorCollection, ErrorResponse } from './output';
 
 export enum GGITErrorClassification {
-    INPUT_VALIDATION_ERROR = 'InputValidationError',
-    API_BUILD_ERROR = 'APIBuildError',
-    ERROR = 'Error',
+  INPUT_VALIDATION_ERROR = 'InputValidationError',
+  API_BUILD_ERROR = 'APIBuildError',
+  ERROR = 'Error',
 }
 
 export class FlatGGITError {
-    private static DEFAULT_ERROR_DETAILS='An unexpected error has occurred';
+  private static readonly defaultErrorDetails =
+    'An unexpected error has occurred';
 
-    public readonly errorDetails: string;
-    public readonly errorMessage?: string;
-    public readonly errorCollectionText?: string;
-    public readonly errorCollectionName: string;
-    public readonly errorClassification: GGITErrorClassification;
+  public readonly errorDetails: string;
 
-    private constructor({
-        errorDetails,
-        errorMessage,
-        errorCollectionText,
-        errorCollectionName,
-        errorClassification
-    }: {
-        errorDetails: string,
-        errorMessage?: string,
-        errorCollectionText?: string,
-        errorCollectionName: string,
-        errorClassification: GGITErrorClassification
-    }) {
-        this.errorDetails = errorDetails;
-        this.errorMessage = errorMessage;
-        this.errorCollectionText = errorCollectionText;
-        this.errorCollectionName = errorCollectionName;
-        this.errorClassification = errorClassification;
-     }
+  public readonly errorMessage?: string;
 
-    public static fromErrorResponse(errorResponse: ErrorResponse): FlatGGITError[] {
-        return [
-            FlatGGITError.flattenGGITErrorCollections(
-              GGITErrorClassification.INPUT_VALIDATION_ERROR,
-              errorResponse.InputErrors?.InputValidationErrors?.ModelRun
-            ),
-            FlatGGITError.flattenGGITErrorCollections(
-              GGITErrorClassification.API_BUILD_ERROR,
-              errorResponse.InputErrors?.APIBuildErrors?.BuildError
-            ),
-            FlatGGITError.flattenGGITErrorCollections(
-              GGITErrorClassification.ERROR,
-              errorResponse.AllErrors?.Errors?.ModelRun
-            ),
-          ].flat();
-    }
+  public readonly errorCollectionText?: string;
 
-    private static flattenGGITErrorCollections(
-        errorClassification: GGITErrorClassification,
-        errorCollections?:
-        | ModelRunErrorCollection
-        | ModelRunErrorCollection[]
-    ): FlatGGITError[] {
-        return [errorCollections ?? []]
-            .flat()
-            .flatMap((modelRunCollection) => {
-                return [modelRunCollection.Error]
-                    .flat()
-                    .map((error) => {
-                        const errorDetails = error?.['@message'] 
-                            ?? modelRunCollection['#text']
-                            ?? FlatGGITError.DEFAULT_ERROR_DETAILS;
-                        return new FlatGGITError({
-                            errorDetails,
-                            errorMessage: error?.['@message'],
-                            errorCollectionText: modelRunCollection['#text'],
-                            errorCollectionName: modelRunCollection['@name'],
-                            errorClassification,
-                        });
-                    });
-            });
-    }
+  public readonly errorCollectionName: string;
+
+  public readonly errorClassification: GGITErrorClassification;
+
+  private constructor({
+    errorDetails,
+    errorMessage,
+    errorCollectionText,
+    errorCollectionName,
+    errorClassification,
+  }: {
+    errorDetails: string;
+    errorMessage?: string;
+    errorCollectionText?: string;
+    errorCollectionName: string;
+    errorClassification: GGITErrorClassification;
+  }) {
+    this.errorDetails = errorDetails;
+    this.errorMessage = errorMessage;
+    this.errorCollectionText = errorCollectionText;
+    this.errorCollectionName = errorCollectionName;
+    this.errorClassification = errorClassification;
+  }
+
+  public static fromErrorResponse(
+    errorResponse: ErrorResponse
+  ): FlatGGITError[] {
+    return [
+      FlatGGITError.flattenGGITErrorCollections(
+        GGITErrorClassification.INPUT_VALIDATION_ERROR,
+        errorResponse.InputErrors?.InputValidationErrors?.ModelRun
+      ),
+      FlatGGITError.flattenGGITErrorCollections(
+        GGITErrorClassification.API_BUILD_ERROR,
+        errorResponse.InputErrors?.APIBuildErrors?.BuildError
+      ),
+      FlatGGITError.flattenGGITErrorCollections(
+        GGITErrorClassification.ERROR,
+        errorResponse.AllErrors?.Errors?.ModelRun
+      ),
+    ].flat();
+  }
+
+  private static flattenGGITErrorCollections(
+    errorClassification: GGITErrorClassification,
+    errorCollections?: ModelRunErrorCollection | ModelRunErrorCollection[]
+  ): FlatGGITError[] {
+    return [errorCollections ?? []].flat().flatMap((modelRunCollection) => {
+      return [modelRunCollection.Error].flat().map((error) => {
+        const errorDetails =
+          error?.['@message'] ??
+          modelRunCollection['#text'] ??
+          FlatGGITError.defaultErrorDetails;
+        return new FlatGGITError({
+          errorDetails,
+          errorMessage: error?.['@message'],
+          errorCollectionText: modelRunCollection['#text'],
+          errorCollectionName: modelRunCollection['@name'],
+          errorClassification,
+        });
+      });
+    });
+  }
 }
--- packages/ggit/src/modeled-ggit-response.ts
+++ packages/ggit/src/modeled-ggit-response.ts
@@ -1,12 +1,13 @@
 import { FlatGGITError } from './ggit-error';
-import { MapUnit, OutputFile, ErrorResponse } from './output';
+import type { MapUnit, OutputFile, ErrorResponse } from './output';
 
 export class ModeledGGITResponse {
-    public readonly daycent: OutputFile<MapUnit>;
-    public readonly errors: FlatGGITError[];
+  public readonly daycent: OutputFile<MapUnit>;
 
-    constructor(response: OutputFile<MapUnit> | ErrorResponse) {
-        this.daycent = (response as OutputFile<MapUnit>);
-        this.errors = FlatGGITError.fromErrorResponse(response as ErrorResponse);
-    }
+  public readonly errors: FlatGGITError[];
+
+  constructor(response: OutputFile<MapUnit> | ErrorResponse) {
+    this.daycent = response as OutputFile<MapUnit>;
+    this.errors = FlatGGITError.fromErrorResponse(response as ErrorResponse);
+  }
 }
